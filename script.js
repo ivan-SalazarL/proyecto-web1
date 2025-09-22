faa0b5eebe4d96f9a21400ee1c8f58810740bbb8
Franklin <fulloac@unsa.edu.pe>
2025-09-21 19:56:32 -0500

    Agregar lógica JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+// Colaborador: Persona B | Colaboró con Persona A y Persona C
+
+document.addEventListener("DOMContentLoaded", () => {
+    const boton = document.getElementById("btnSaludo");
+
+    boton.addEventListener("click", () => {
+        alert("Hola desde la lógica (Persona B)!");
+    });
+});
